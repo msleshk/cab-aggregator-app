1398ab59b7dff738559f3758c87b6b5a3491e804
Maksim Leshko <msleshko2003@gmail.com>
2025-02-14 14:07:03 +0300

    Initial commit

---
+++ rides-service/src/test/java/com/example/ridesservice/RidesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ridesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
