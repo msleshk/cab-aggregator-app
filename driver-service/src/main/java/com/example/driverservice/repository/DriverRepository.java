0e9d3141e2937550d172c7e8c55c02396b890f9e
Maksim Leshko <msleshko2003@gmail.com>
2025-02-20 18:58:00 +0300

    Add repository

---
+++ driver-service/src/main/java/com/example/driverservice/repository/DriverRepository.java
@@ -0,0 +1,9 @@
+package com.example.driverservice.repository;
+
+import com.example.driverservice.model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
