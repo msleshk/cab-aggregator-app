f6607b0998258c7e07376a7e826c5818c045285f
Maksim Leshko <msleshko2003@gmail.com>
2025-02-21 14:15:54 +0300

    Add services

---
+++ driver-service/src/main/java/com/example/driverservice/service/DriverService.java
@@ -0,0 +1,14 @@
+package com.example.driverservice.service;
+
+import com.example.driverservice.dto.driver.DriverRequest;
+import com.example.driverservice.dto.driver.DriverResponse;
+
+import java.util.List;
+
+public interface DriverService {
+    void addDriver(DriverRequest driverDto);
+    void deleteDriver(Long id);
+    void updateDriver(Long id, DriverRequest driverDto);
+    List<DriverResponse> getAllDrivers();
+    DriverResponse getDriverById(Long id);
+}
