106bfbf6ddb9a13fd87f4db8a14594a77646d63a
Maksim Leshko <msleshko2003@gmail.com>
2025-02-23 20:34:22 +0300

    Fix logic in services, add custom exceptions in there, fix repos

--- driver-service/src/main/java/com/example/driverservice/repository/DriverRepository.java
+++ driver-service/src/main/java/com/example/driverservice/repository/DriverRepository.java
@@ -10,7 +10,10 @@ import java.util.Optional;
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
     List<Driver> findAllByDeletedFalse();
+
     Optional<Driver> findByIdAndDeletedFalse(Long id);
+
     Optional<Driver> findDriverByEmailAndDeletedFalse(String email);
+
     Optional<Driver> findDriverByPhoneNumberAndDeletedFalse(String email);
 }
--- driver-service/src/main/java/com/example/driverservice/service/implementation/DriverServiceImpl.java
+++ driver-service/src/main/java/com/example/driverservice/service/implementation/DriverServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.driverservice.service.implementation;
 
 import com.example.driverservice.dto.driver.DriverRequest;
 import com.example.driverservice.dto.driver.DriverResponse;
+import com.example.driverservice.exception.EntityNotFoundException;
+import com.example.driverservice.exception.ResourceAlreadyTakenException;
 import com.example.driverservice.model.Car;
 import com.example.driverservice.model.Driver;
 import com.example.driverservice.repository.CarRepository;
@@ -31,13 +33,18 @@ public class DriverServiceImpl implements DriverService {
     @Transactional
     public void addDriver(DriverRequest driverDto) {
         checkIfDriverUnique(driverDto);
+        if (driverDto.getCarId() != null) {
+            checkIfCarTaken(driverDto.getCarId());
+        }
         driverRepository.save(driverMapper.toEntity(driverDto));
     }
 
     @Override
     @Transactional
     public void deleteDriver(Long id) {
-        Driver driver = driverRepository.findByIdAndDeletedFalse(id).orElseThrow();//TODO custom exc
+        Driver driver = driverRepository.findByIdAndDeletedFalse(id)
+                .orElseThrow(() -> new EntityNotFoundException("Driver with this id not found!"));
+        driver.setCar(null);
         driver.setDeleted(true);
         driverRepository.save(driver);
     }
@@ -45,13 +52,15 @@ public class DriverServiceImpl implements DriverService {
     @Override
     @Transactional
     public void updateDriver(Long id, DriverRequest driverDto) {
-        Driver driverToUpdate = driverRepository.findByIdAndDeletedFalse(id).orElseThrow();//todo exc
-        if (!driverToUpdate.getEmail().equals(driverDto.getEmail())){
+        Driver driverToUpdate = driverRepository.findByIdAndDeletedFalse(id)
+                .orElseThrow(() -> new EntityNotFoundException("Driver with this id not found!"));
+        if (!driverToUpdate.getEmail().equals(driverDto.getEmail())) {
             checkIfEmailUnique(driverDto.getEmail());
         }
-        if (!driverToUpdate.getPhoneNumber().equals(driverDto.getPhoneNumber())){
+        if (!driverToUpdate.getPhoneNumber().equals(driverDto.getPhoneNumber())) {
             checkIfPhoneUnique(driverDto.getPhoneNumber());
         }
+        checkIfCarTaken(driverDto.getCarId());
 
         driverToUpdate.setName(driverDto.getName());
         driverToUpdate.setEmail(driverDto.getEmail());
@@ -69,24 +78,36 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public DriverResponse getDriverById(Long id) {
-        return driverRepository.findByIdAndDeletedFalse(id).map(driverMapper::toDto).orElseThrow();//todo exc
+        return driverRepository.findByIdAndDeletedFalse(id).map(driverMapper::toDto)
+                .orElseThrow(() -> new EntityNotFoundException("Driver with this id not found!"));
     }
 
     private void checkIfDriverUnique(DriverRequest driverRequest) {
         checkIfEmailUnique(driverRequest.getEmail());
         checkIfPhoneUnique(driverRequest.getPhoneNumber());
     }
-    private void checkIfPhoneUnique(String phoneNumber){
-        if(driverRepository.findDriverByPhoneNumberAndDeletedFalse(phoneNumber).isPresent()){
-            // todo already taken exc
+
+    private void checkIfPhoneUnique(String phoneNumber) {
+        if (driverRepository.findDriverByPhoneNumberAndDeletedFalse(phoneNumber).isPresent()) {
+            throw new ResourceAlreadyTakenException("This phone number already taken!");
         }
     }
-    private void checkIfEmailUnique(String email){
-        if(driverRepository.findDriverByEmailAndDeletedFalse(email).isPresent()){
-            //todo already taken exc
+
+    private void checkIfEmailUnique(String email) {
+        if (driverRepository.findDriverByEmailAndDeletedFalse(email).isPresent()) {
+            throw new ResourceAlreadyTakenException("This email already taken!");
         }
     }
-    private Car findCarById(Long carId){
-        return carRepository.findCarByIdAndDeletedFalse(carId).orElseThrow();//todo exc
+
+    private Car findCarById(Long carId) {
+        return carRepository.findCarByIdAndDeletedFalse(carId)
+                .orElseThrow(() -> new EntityNotFoundException("Car with this id not found!"));
+    }
+
+    private void checkIfCarTaken(Long carId) {
+        Car car = findCarById(carId);
+        if (car.getDriver() != null) {
+            throw new ResourceAlreadyTakenException("This car already have owner!");
+        }
     }
 }
