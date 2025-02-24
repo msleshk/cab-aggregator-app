f56dcb96af1c3b156caa997b298ea0fc42daae77
Maksim Leshko <msleshko2003@gmail.com>
2025-02-24 18:35:10 +0300

    Update luqiubase and repo

--- ride-service/src/main/java/cab/app/rideservice/repository/RideRepository.java
+++ ride-service/src/main/java/cab/app/rideservice/repository/RideRepository.java
@@ -1,9 +1,18 @@
 package cab.app.rideservice.repository;
 
 import cab.app.rideservice.model.Ride;
+import cab.app.rideservice.model.enums.RideStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RideRepository extends JpaRepository<Ride, Long> {
+    Optional<Ride> findByIdAndDeletedFalse(Long id);
+    List<Ride> findAllByDeletedFalse();
+    List<Ride> findAllByDeletedFalseAndStatus(RideStatus rideStatus);
+    List<Ride> findAllByDeletedFalseAndPassengerId(Long driverId);
+    List<Ride> findAllByDeletedFalseAndDriverId(Long driverId);
 }
