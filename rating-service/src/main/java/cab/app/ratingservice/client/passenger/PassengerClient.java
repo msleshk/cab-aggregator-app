bf0d25a0f8f3a9cc64393a3017a232bf8da4c4e1
Maksim Leshko <msleshko2003@gmail.com>
2025-04-08 12:54:09 +0300

    Add eureka client in rating service

--- rating-service/src/main/java/cab/app/ratingservice/client/passenger/PassengerClient.java
+++ rating-service/src/main/java/cab/app/ratingservice/client/passenger/PassengerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${feign.client.name.passenger}", url = "${feign.client.path.passenger}", configuration = FeignConfig.class)
+@FeignClient(name = "${feign.client.name.passenger}", path = "${feign.client.path.passenger}", configuration = FeignConfig.class)
 public interface PassengerClient {
     @GetMapping("{id}")
     void getPassengerById(@PathVariable("id") Long passengerId);
